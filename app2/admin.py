9cec74d2634f035e471406fff7af0a7c52f8e6a9
adylbek0408 <118812973+adylbek0408@users.noreply.github.com>
2024-04-28 15:30:29 +0300

    Update admin.py

--- app2/admin.py
+++ app2/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(RelatedServices)
+admin.site.register(AdvantagesM2M)
+admin.site.register(Package)
+admin.site.register(Questions)
+
+
+class StatusNumberInline(admin.TabularInline):
+    model = StatusNumber
+    extra = 1
+
+
+@admin.register(CodeNumber)
+class StatusNumberAdmin(admin.ModelAdmin):
+    inlines = [StatusNumberInline]
+
+
+class NextTariffInternetM2MInline(admin.TabularInline):
+    model = NextTariffInternetM2M
+    extra = 1
+
+
+@admin.register(TariffInternetM2M)
+class TariffInternetM2MAdmin(admin.ModelAdmin):
+    inlines = [NextTariffInternetM2MInline]
+
