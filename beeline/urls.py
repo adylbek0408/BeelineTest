22f19f3bf0f9e644b797d56d98ec407984c4fde8
adylbek0408 <118812973+adylbek0408@users.noreply.github.com>
2024-04-28 14:25:04 +0300

    Update urls.py

--- beeline/urls.py
+++ beeline/urls.py
@@ -37,7 +37,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app1/', include('app1.urls')),
-    # path('app2/', include('app2.urls')),
+    path('app2/', include('app2.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
